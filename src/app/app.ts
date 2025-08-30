154e9e766a04c48e3b0d0f1532422aafc0fc6cdf
aisge <g.aistleitner@htl-leonding.ac.at>
2025-08-30 19:19:41 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,67 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('reactive-form-template');
+  submitted = false;
+  form!: FormGroup;
+
+  constructor() {}
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    const value = this.form.getRawValue(); // typed & non-null
+    console.log('Submit: ', value);
+  }
+
+  reset() {
+    this.submitted = false;
+    this.form.reset({
+      firstname: '',
+      lastname: '',
+      email: '',
+      address: {street: '', city: ''}
+    });
+  }
+
+  //<editor-fold desc="// Helper methods ...">
+  get fnameCtrl() {
+    return this.form.controls['firstname'];
+  }
+
+  get lnameCtrl() {
+    return this.form.controls['lastname'];
+  }
+
+  get emailCtrl() {
+    return this.form.controls['email'];
+  }
+
+  get addressGroup() {
+    return this.form.controls['address'] as FormGroup;
+  }
+
+  get streetCtrl() {
+    return this.addressGroup.controls['street'];
+  }
+
+  get cityCtrl() {
+    return this.addressGroup.controls['city'];
+  }
+
+
+  shouldShowInvalid(ctrl: { invalid: boolean; touched: boolean; dirty: boolean }) {
+    return ctrl.invalid && (ctrl.touched || ctrl.dirty || this.submitted);
+  }
+  //</editor-fold>
+
 }
